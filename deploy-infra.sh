9d6f9bb9de61e630b6c5c491d1830aa04d2632c6
Your Name <you@example.com>
2022-06-10 14:16:30 +0000

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -13,7 +13,7 @@ GH_BRANCH=master
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
-Echo $CODEPIPELINE_BUCKET
+echo $CODEPIPELINE_BUCKET
 
 # Deploys static resources
 echo "\n\n=========== Deploying setup.yml ==========="
